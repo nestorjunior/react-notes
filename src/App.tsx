d0b51b050a5586bba046e6f9f57a87d2997dd965
Nestor Júnior <nestorjunior@MacBook-Pro-de-Nestor.local>
2025-04-07 15:09:43 -0300

    init project structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Details } from './pages/Details'
+
+function App() {
+
+  return (
+    <>
+      <h2>React Notes</h2>
+      <Details />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles'
+
+export function Details() {
+  return (
+    <Container>
+      <h3 className='teste'>Details</h3>
+    </Container>
+  );
+}
